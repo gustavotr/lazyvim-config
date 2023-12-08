a326b94b63cac177080ffd18aa438d1262ec1e07
Gustavo T. Rudiger <gustavo@trudax.tech>
2023-12-08 16:11:21 -0300

    feat: 🎸 add shortcut to open files with default SO application

--- lua/plugins/navigation.lua
+++ lua/plugins/navigation.lua
@@ -1,3 +1,23 @@
+function openFileWithDefaultApp(file_path)
+  local os_name = string.lower(package.config:sub(1, 1)) == "\\" and "windows" or "unix"
+  local open_command
+
+  if os_name == "windows" then
+    open_command = 'start "" "' .. file_path .. '"'
+  elseif os_name == "unix" then
+    open_command = 'xdg-open "' .. file_path .. '"'
+  else
+    print("Unsupported operating system")
+    return
+  end
+
+  local success, _, exit_code = os.execute(open_command)
+
+  if not success or exit_code ~= 0 then
+    print("Failed to open file")
+  end
+end
+
 return {
   {
     "nvim-neo-tree/neo-tree.nvim",
@@ -6,6 +26,17 @@ return {
         filtered_items = {
           hide_dotfiles = false,
           hide_gitignored = false,
+        },
+        commands = {
+          open_with_system = function(state)
+            local path = state.tree:get_node().path
+            openFileWithDefaultApp(path)
+          end,
+        },
+        window = {
+          mappings = {
+            ["O"] = "open_with_system",
+          },
         },
       },
     },
