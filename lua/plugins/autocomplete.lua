21d6d91a471fa3438ccdee3de1b8fb823fc82e95
Gustavo T. Rudiger <gustavo@trudax.tech>
2023-12-15 17:07:44 -0300

    fix: 🐛 freezing when typing /

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -17,12 +17,12 @@ return {
   --     end, { expr = true })
   --   end,
   -- },
-  -- {
-  --   "nvim-cmp",
-  --   opts = function(_, opts)
-  --     opts.sources = vim.tbl_filter(function(path)
-  --       return not vim.tbl_contains({ "path" }, path.name)
-  --     end, opts.sources)
-  --   end,
-  -- },
+  {
+    "nvim-cmp",
+    opts = function(_, opts)
+      opts.sources = vim.tbl_filter(function(path)
+        return not vim.tbl_contains({ "path" }, path.name)
+      end, opts.sources)
+    end,
+  },
 }
