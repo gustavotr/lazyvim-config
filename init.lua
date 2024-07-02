47df12f48c6e705fa495077ab5b772795a3c7f8e
Gustavo T. Rudiger <gustavo@trudax.tech>
2024-07-02 18:10:37 +0100

    feat: 🎸 specify node version to be used by neovim

--- init.lua
+++ init.lua
@@ -1,2 +1,9 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
+
+-- set node version to be used by neovim
+local home_dir = "/home/gustavotr/"
+local node_bin = ".nvm/versions/node/v20.11.1/bin"
+vim.g.node_host_prog = home_dir .. node_bin .. "/node"
+vim.cmd("let $PATH = '" .. home_dir .. node_bin .. ":' . $PATH")
+
 require("config.lazy")
