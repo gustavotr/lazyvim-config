8cf8c872fe6d914d70a88c6e9c30adfdabe0b735
Gustavo T. Rudiger <gustavo@trudax.tech>
2025-08-20 22:52:32 -0300

    feat: 🎸 enable denols on deno files only

--- init.lua
+++ init.lua
@@ -1,9 +1,9 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 
 -- set node version to be used by neovim
-local home_dir = "/home/gustavotr/"
-local node_bin = ".nvm/versions/node/v22.14.0/bin"
-vim.g.node_host_prog = home_dir .. node_bin .. "/node"
-vim.cmd("let $PATH = '" .. home_dir .. node_bin .. ":' . $PATH")
+local node_path = vim.fn.systemlist("which node")[1]
+local node_bin = string.sub(node_path, 1, -6)
+vim.g.node_host_prog = node_path
+vim.cmd("let $PATH = '" .. node_bin .. ":' . $PATH")
 
 require("config.lazy")
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,7 +1,27 @@
+local nvim_lsp = require("lspconfig")
+
 return {
   {
     "neovim/nvim-lspconfig",
     opts = {
+      servers = {
+        denols = {
+          filetypes = { "typescript", "typescriptreact" },
+          root_dir = function(...)
+            return nvim_lsp.util.root_pattern("deno.jsonc", "deno.json")(...)
+          end,
+        },
+        vtsls = {
+          single_file_support = false,
+          root_dir = function()
+            return not vim.fs.root(0, { 'deno.json', 'deno.jsonc' })
+                and vim.fs.root(0, {
+                  'tsconfig.json',
+                  'package.json',
+                })
+          end,
+        },
+      },
       setup = {
         tsserver = function()
           require("lazyvim.util").lsp.on_attach(function(client)
