c0befbdb36fc9e747169d8418a86da1f75a05b5a
Gustavo T. Rudiger <gustavo@trudax.tech>
2025-10-15 16:15:46 -0300

    feat: 🎸 enable sidekick

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,38 +1,38 @@
 local nvim_lsp = require("lspconfig")
 
 return {
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        denols = {
-          filetypes = { "typescript", "typescriptreact" },
-          root_dir = function(...)
-            return nvim_lsp.util.root_pattern("deno.jsonc", "deno.json")(...)
-          end,
-        },
-        vtsls = {
-          single_file_support = false,
-          root_dir = function()
-            return not vim.fs.root(0, { 'deno.json', 'deno.jsonc' })
-                and vim.fs.root(0, {
-                  'tsconfig.json',
-                  'package.json',
-                })
-          end,
-        },
-      },
-      setup = {
-        tsserver = function()
-          require("lazyvim.util").lsp.on_attach(function(client)
-            if client.name == "tsserver" then
-              client.server_capabilities.documentFormattingProvider = false
-            end
-          end)
-        end,
-      },
-    },
-  },
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   opts = {
+  --     servers = {
+  --       denols = {
+  --         filetypes = { "typescript", "typescriptreact" },
+  --         root_dir = function(...)
+  --           return nvim_lsp.util.root_pattern("deno.jsonc", "deno.json")(...)
+  --         end,
+  --       },
+  --       vtsls = {
+  --         single_file_support = false,
+  --         root_dir = function()
+  --           return not vim.fs.root(0, { 'deno.json', 'deno.jsonc' })
+  --               and vim.fs.root(0, {
+  --                 'tsconfig.json',
+  --                 'package.json',
+  --               })
+  --         end,
+  --       },
+  --     },
+  --     setup = {
+  --       tsserver = function()
+  --         require("lazyvim.util").lsp.on_attach(function(client)
+  --           if client.name == "tsserver" then
+  --             client.server_capabilities.documentFormattingProvider = false
+  --           end
+  --         end)
+  --       end,
+  --     },
+  --   },
+  -- },
   {
     "akinsho/flutter-tools.nvim",
     lazy = false,
